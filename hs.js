7a8ddaa03e480fba75c3f52bd4ac4b7335242576
Julien Moumné <julien@moumne.com>
2016-04-09 14:17:00 +0200

    add a script to generate demos & update demos

--- hs.js
+++ hs.js
@@ -1,24 +1,43 @@
-item({desc: 'hotshell-web'}, function() {
+item({desc: 'hotshell website'}, function() {
 
-    item({key: 's', desc: 'jekyll serve', cmd: 'jekyll serve'})
+    linux = exec('uname').indexOf('Linux') > -1
+    browser = linux ? 'sensible-browser' : 'open'
+    hotshellDir = '$GOPATH/src/github.com/julienmoumne/hotshell'
 
-    item({key: 'g', desc: 'generate demos'}, function() {
+    item({key: 's', cmd: 'jekyll serve'})
 
-        item({key: 'f', desc: 'demo', cmd: 'hs --generate-demo -f _includes/demo.hs.js > demos/demo.hs.js.html'})
-        item({key: 'u', desc: 'tutorial', cmd: 'hs --generate-demo -f demos/tutorial.hs.js > demos/tutorial.hs.js.html'})
-        item({key: 'h', desc: 'hotshell', cmd: demo('hs.js', 'hs.js')})
-        item({key: 'd', desc: 'default', cmd: demo('examples/default/default.hs.js', 'default.hs.js')})
-        item({key: 'r', desc: 'docker', cmd: demo('examples/docker/docker.hs.js', 'docker.hs.js')})
-        item({key: 'c', desc: 'docker-compose', cmd: demo('examples/docker/docker-compose.hs.js', 'docker-compose.hs.js')})
-        item({key: 'm', desc: 'docker-machine', cmd: demo('examples/docker/docker-machine.hs.js', 'docker-machine.hs.js')})
-        item({key: 'v', desc: 'vagrant', cmd: demo('examples/vagrant/vagrant.hs.js', 'vagrant.hs.js')})
-        item({key: 't', desc: 'topten', cmd: demo('examples/topten/topten.hs.js', 'topten.hs.js')})
+    item({key: 'd', desc: 'demos'}, function() {
 
-        function demo(src, out) {
-            return 'hs --chdir --generate-demo ' +
-                '-f $GOPATH/src/github.com/julienmoumne/hotshell/' + src +
-                '> demos/'+ out +'.html'
-        }
+        item({key: 'g', desc: 'generate\n  ', cmd: script(
+            'set -e',
+            'export DOCKER_MACHINE_NAME=dev',
+            generateDemo('_includes/demo.hs.js', 'demo.hs.js'),
+            generateDemo('demos/tutorial.hs.js', 'tutorial.hs.js'),
+            generateDemo(fromHotshell('hs.js'), 'hs.js'),
+            generateDemo(fromHotshell('examples/default/default.hs.js'), 'default.hs.js'),
+            generateDemo(fromHotshell('examples/docker/docker.hs.js'), 'docker.hs.js'),
+            generateDemo(fromHotshell('examples/docker/docker-compose.hs.js'), 'docker-compose.hs.js'),
+            generateDemo(fromHotshell('examples/docker/docker-machine.hs.js'), 'docker-machine.hs.js'),
+            generateDemo(fromHotshell('examples/vagrant/vagrant.hs.js'), 'vagrant.hs.js'),
+            generateDemo(fromHotshell('examples/topten/topten.hs.js'), 'topten.hs.js')
+        )})
+
+        item({key: 'o', desc: 'open demos', cmd: 'find demos  -name "*.html" -exec ' + browser + ' {} \\;'})
     })
 
+    item({key: 'h', desc: 'hotshell', cmd: 'hs --chdir -f ' + hotshellDir + '/hs.js'})
 })
+
+function generateDemo(src, out) {
+    return 'hs --chdir --generate-demo -f ' + src + ' > demos/'+ out +'.html'
+}
+
+function fromHotshell(file) {
+    return hotshellDir + '/' + file
+}
+
+function script () {
+    src = '';
+    _.each(arguments, function (el, ix) { src += '   ' + el + '\n' })
+    return src
+}

818a4e4e0e4303cede29bdbad93d8442df016797
Julien Moumné <julien@moumne.com>
2016-06-24 19:34:41 +0200

    formatting

--- _includes/demo.hs.js
+++ _includes/demo.hs.js
@@ -1,12 +1,12 @@
-item({desc: 'hotshell demo'}, function() {
+item({desc: 'hotshell demo'}, function () {
 
-  // command items
-  item({key: 'r', desc: 'restart apache', cmd: 'sudo service apache2 restart'})
-  item({key: 's', desc: 'synchronize time', cmd: 'sudo /usr/sbin/ntpdate pool.ntp.org'})
-  
-  // submenu to group log related commands
-  item({key: 'l', desc: 'apache logs'}, function() {
-    item({key: 'a', desc: 'access.log', cmd: 'less +F /var/log/apache2/access.log'})
-    item({key: 'e', desc: 'error.log', cmd: 'less +F /var/log/apache2/error.log'})
-  })
+    // command items
+    item({key: 'r', desc: 'restart apache', cmd: 'sudo service apache2 restart'})
+    item({key: 's', desc: 'synchronize time', cmd: 'sudo /usr/sbin/ntpdate pool.ntp.org'})
+
+    // submenu to group log related commands
+    item({key: 'l', desc: 'apache logs'}, function () {
+        item({key: 'a', desc: 'access.log', cmd: 'less +F /var/log/apache2/access.log'})
+        item({key: 'e', desc: 'error.log', cmd: 'less +F /var/log/apache2/error.log'})
+    })
 })  
--- hs.js
+++ hs.js
@@ -1,25 +1,25 @@
-item({desc: 'hotshell website'}, function() {
+item({desc: 'hotshell website'}, function () {
 
     linux = exec('uname').indexOf('Linux') > -1
-    browser = linux ? 'sensible-browser' : 'open'
+    browser = linux ? 'sensible-browser' : 'open'
     hotshellDir = '$GOPATH/src/github.com/julienmoumne/hotshell'
 
     item({key: 's', cmd: 'jekyll serve'})
 
-    item({key: 'd', desc: 'demos'}, function() {
+    item({key: 'd', desc: 'demos'}, function () {
 
         item({key: 'g', desc: 'generate\n  ', cmd: script(
-            'set -e',
-            generateDemo('_includes/demo.hs.js', 'demo.hs.js'),
-            generateDemo('demos/tutorial.hs.js', 'tutorial.hs.js'),
-            generateDemo(fromHotshell('hs.js'), 'hs.js'),
-            generateDemo(fromHotshell('examples/default/default.hs.js'), 'default.hs.js'),
-            generateDemo(fromHotshell('examples/docker/docker.hs.js'), 'docker.hs.js'),
-            generateDemo(fromHotshell('examples/docker/docker-compose.hs.js'), 'docker-compose.hs.js'),
-            'DOCKER_MACHINE_NAME=dev ' + generateDemo(fromHotshell('examples/docker/docker-machine.hs.js'), 'docker-machine.hs.js'),
-            generateDemo(fromHotshell('examples/vagrant/vagrant.hs.js'), 'vagrant.hs.js'),
-            generateDemo(fromHotshell('examples/topten/topten.hs.js'), 'topten.hs.js'),
-            generateDemo(fromHotshell('examples/network/network.hs.js'), 'network.hs.js')
+                'set -e',
+                generateDemo('_includes/demo.hs.js', 'demo.hs.js'),
+                generateDemo('demos/tutorial.hs.js', 'tutorial.hs.js'),
+                generateDemo(fromHotshell('hs.js'), 'hs.js'),
+                generateDemo(fromHotshell('examples/default/default.hs.js'), 'default.hs.js'),
+                generateDemo(fromHotshell('examples/docker/docker.hs.js'), 'docker.hs.js'),
+                generateDemo(fromHotshell('examples/docker/docker-compose.hs.js'), 'docker-compose.hs.js'),
+                'DOCKER_MACHINE_NAME=dev ' + generateDemo(fromHotshell('examples/docker/docker-machine.hs.js'), 'docker-machine.hs.js'),
+                generateDemo(fromHotshell('examples/vagrant/vagrant.hs.js'), 'vagrant.hs.js'),
+                generateDemo(fromHotshell('examples/topten/topten.hs.js'), 'topten.hs.js'),
+                generateDemo(fromHotshell('examples/network/network.hs.js'), 'network.hs.js')
         )})
 
         item({key: 'o', desc: 'open demos', cmd: 'find demos  -name "*.html" -exec ' + browser + ' {} \\;'})
@@ -29,15 +29,17 @@ item({desc: 'hotshell website'}, function() {
 })
 
 function generateDemo(src, out) {
-    return 'hs --chdir --generate-demo -f ' + src + ' > demos/'+ out +'.html'
+    return 'hs --chdir --generate-demo -f ' + src + ' > demos/' + out + '.html'
 }
 
 function fromHotshell(file) {
     return hotshellDir + '/' + file
 }
 
-function script () {
+function script() {
     src = '';
-    _.each(arguments, function (el, ix) { src += '   ' + el + '\n' })
+    _.each(arguments, function (el) {
+        src += '   ' + el + '\n'
+    })
     return src
 }

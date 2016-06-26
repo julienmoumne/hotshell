feec423ec09658373b0522b6ca4b4fc8803df722
Julien Moumné <julien@moumne.com>
2016-06-26 18:23:19 +0200

    more robust test when an exception occurs in a nested closure

--- cmd/hs/testcases/dsl-errors/error-in-nested-closure/hs.js
+++ cmd/hs/testcases/dsl-errors/error-in-nested-closure/hs.js
@@ -2,6 +2,9 @@ item({desc: 'Runtime Error in nested closure'}, function () {
 
     item({desc: 'nested closure'}, function () {
 
+        item({key: 's', desc: 'skipped'})
         throw new Error('Runtime Error')
     })
+
+    item({key: 'n', desc: 'not skipped', cmd: 'echo not skipped'})
 })

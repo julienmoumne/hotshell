4c33b03fcb173f94dab6adad0b9a38f7f0d8e4ba
Julien Moumné <julien@moumne.com>
2017-05-10 16:00:22 +0200

    doc: more concise network.hs.js example

--- examples/network/network.hs.js
+++ examples/network/network.hs.js
@@ -1,11 +1,9 @@
 item({desc: 'network'}, function () {
 
-    item({key: 'e', desc: 'eth0'}, function () {
-        interface('eth0')
-    })
+    item({key: 'e', desc: 'eth0'}, interface('eth0'))
 
     item({key: 'w', desc: 'wlan0'}, function () {
-        interface('wlan0')
+        interface('wlan0')()
         // specific commands can be added when needed
         item({key: 'l', desc: 'list access points', cmd: 'iwlist scan'})
     })
@@ -13,8 +11,10 @@ item({desc: 'network'}, function () {
     function interface(id) {
         var ifconfig = 'ifconfig ' + id
         var sudo = 'sudo ' + ifconfig
-        item({key: 'i', desc: 'info', cmd: ifconfig})
-        item({key: 'u', desc: 'up', cmd: sudo + ' up'})
-        item({key: 'd', desc: 'down', cmd: sudo + ' down'})
+        return function() {
+            item({key: 'i', desc: 'info', cmd: ifconfig})
+            item({key: 'u', desc: 'up', cmd: sudo + ' up'})
+            item({key: 'd', desc: 'down', cmd: sudo + ' down'})
+        }
     }
 })

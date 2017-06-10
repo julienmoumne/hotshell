0f0a1293ea664b16c0881cb2da6e1b10c76b1a11
Julien Moumné <julien@moumne.com>
2017-06-10 21:16:44 +0200

    doc: update --help & man

--- cmd/options/options.go
+++ cmd/options/options.go
@@ -2,7 +2,7 @@ package options
 
 type Options struct {
 	Default      bool   `long:"default" description:"Load the default menu to get to know Hotshell"`
-	File         string `short:"f" long:"file" description:"Specify an alternate definition file (defaults: ./hs.js, ~/.hs/hs.js)" value-name:"FILE|DIR"`
+	File         string `short:"f" long:"file" description:"Specify an alternate definition file (default: ./hs.js)" value-name:"FILE"`
 	GenerateDemo bool   `long:"generate-demo" description:"Generate an interactive HTML demo to stdout"`
 	GenerateMd   bool   `long:"generate-md" description:"Generate a markdown documentation of the menu to stdout"`
 	Version      bool   `short:"v" long:"version" description:"Print version information and quit"`
